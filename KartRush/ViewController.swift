21741e653bd21b8bfcc31cf56538be548777674d
user148325 <user148325@tx014.local>
2019-12-01 11:11:21 -0600

    Initial Commit

---
+++ KartRush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KartRush
+//
+//  Created by user148325 on 12/1/19.
+//  Copyright © 2019 user148325. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
